59e9672d0489d0974ee36284fb6a83af137826d5
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-21 22:26:24 +0300

    functioning to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+
+let tasks = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const input = document.getElementById("new-task-description");
+  const task = input.value;
+
+  if (task) {
+    tasks.push(task);
+    updateTaskList();
+    input.value = "";
+  }
+});
+
+function updateTaskList() {
+  taskList.innerHTML = "";
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.innerText = task;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => {
+      tasks = tasks.filter((t) => t !== task);
+      updateTaskList();
+    });
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+  });
+}
+
 });
